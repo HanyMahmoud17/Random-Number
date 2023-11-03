218b46399f6549392bac407c35cab11d8caf47cd
hamymah <hanymah823@gmail.com>
2023-11-03 19:25:02 +0200

    now i make a guess number and check if this number is higher or lower the user number and make button to make + or _

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -2,6 +2,8 @@ import { View,Text,StyleSheet } from "react-native";
 import Title from "../components/ui/Title";
 import { useState } from "react";
 import NumberContainer from "../components/game/NumberContainer";
+import PrimaryButton from "../components/ui/PrimaryButton";
+import { Alert } from "react-native";
 
 function generateRandomNumner(max,min, exclude){
     const rndNum=Math.floor(Math.random() * (max-min) ) + min ;
@@ -13,14 +15,40 @@ if(rndNum === exclude){
 }
 }
 
+let minNumber=1;
+let maxNumber=100;
+
 function GameScreen({userNumber}){
-    const initialGuess=generateRandomNumner(1,100,userNumber)
+    const initialGuess=generateRandomNumner(minNumber,maxNumber,userNumber)
     const[currentGuess,setCurrentGuess]=useState(initialGuess)
+
+    function nextGuessNumber(direction){
+        if((direction==='lower'&& currentGuess < userNumber) || (direction==='higher'&& currentGuess > userNumber)){
+            Alert.alert("Don't lie",'you show click on the right button...',
+            [{text:'Sorry!',style:'cancel'}]
+            )
+            return;
+        }
+        if(direction === 'lower'){
+            maxNumber=currentGuess
+        }else {
+            minNumber=currentGuess+1
+        }
+        const newRandomNumber=generateRandomNumner(minNumber,maxNumber,currentGuess)
+        setCurrentGuess(newRandomNumber)
+    }
 
 return (
     <View style={styles.mainContainer}>
         <Title>Opponent's Guess</Title>
         <NumberContainer>{currentGuess}</NumberContainer>
+        <View>
+            <Text>Higher or Lower ? </Text>
+            <View>
+                <PrimaryButton onPress={nextGuessNumber.bind(this,'higher')}>+</PrimaryButton>
+                <PrimaryButton onPress={nextGuessNumber.bind(this,'lower')}>-</PrimaryButton>
+            </View>
+        </View>
     </View>
 )
 }
