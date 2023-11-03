d3e65622ab53a1319754831969b0a14ef39cf725
hamymah <hanymah823@gmail.com>
2023-11-03 11:42:07 +0200

    now i add the compnent og imagebackground and make it shammer into graidient

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StyleSheet, View } from 'react-native';
 import {LinearGradient} from 'expo-linear-gradient'
 import MainScreen from './screens/MainScreen';
+import { ImageBackground } from 'react-native';
 
 export default function App() {
   return (
     <LinearGradient colors={['#4e0329','#ddb52f']} style={styles.rootScreen}>
-      <MainScreen/>
+      <ImageBackground style={styles.rootScreen} imageStyle={styles.backgroundImage} source={require('./assets/images/background.jpg')} resizeMode='cover'>
+        <MainScreen/>
+      </ImageBackground>
     </LinearGradient>
   ); 
 }
@@ -13,6 +16,9 @@ export default function App() {
 const styles = StyleSheet.create({
   rootScreen:{
     flex:1,
-    // backgroundColor:'#ddb52f'
+    // backgroundColor:'#ddb52f',
+  },
+  backgroundImage:{
+    opacity:0.25
   }
 });
